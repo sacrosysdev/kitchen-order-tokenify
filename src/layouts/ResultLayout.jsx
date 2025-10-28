cb17daf2b1bf842aec96a4372c9e5de98687d0f2
Shinas Muhammed <shinasmuhd43@gmail.com>
2025-10-28 17:31:29 +0530

    navigations added

--- src/layouts/ResultLayout.jsx
+++ src/layouts/ResultLayout.jsx
@@ -7,7 +7,7 @@ const ResultLayout = ({children}) => {
     <div className="min-h-screen bg-cover bg-center bg-no-repeat font-montserrat relative pb-10"
         style={{ backgroundImage: `url(${Bg})` }}>
         <ReadyNav/>
-        <div className='absolute inset-0 bg-white/10'></div>
+        <div className='absolute inset-0 bg-white/10 pointer-events-none'></div>
         <div className=''>
             {children}
         </div>
