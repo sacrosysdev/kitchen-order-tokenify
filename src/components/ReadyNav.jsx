36ac643df69c53f0a1e2bbc6cbe01bba9700b482
Shinas Muhammed <shinasmuhd43@gmail.com>
2025-10-29 12:48:56 +0530

    height alterations

--- src/components/ReadyNav.jsx
+++ src/components/ReadyNav.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const ReadyNav = () => {
     return (
-        <div className='text-black bg-[#F1F1F1] border-b border-[#404040] flex items-center justify-between py-2 px-5'>
+        <div className='text-black bg-[#F1F1F1] border-b border-[#404040] flex items-center justify-between py-1 px-5'>
             <div className='flex items-center gap-5 text-[#121212]'>
                 <div className="bg-linear-to-r from-[#F97316] to-[#EF4444] font-pacifico w-12 h-12 2xl:w-16 2xl:h-16 rounded-full flex justify-center items-center">
                     <span className='text-xl'>l</span>
